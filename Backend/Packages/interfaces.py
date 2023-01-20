79ef4c31beee52aaaabe76d0f952c55e8eb112f6
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-20 22:48:06 +0500

    Interfaces Setup Completed

--- Backend/Packages/interfaces.py
+++ Backend/Packages/interfaces.py
@@ -8,18 +8,19 @@ class Interfaces(Resource):
 
         for interface in interfaces:
             interface_json = {}
-            interface_json["name"] = interface
-            interface_json["addresses"] = []
-
-            for address in interfaces[interface]:
-                address_json = {}
-                address_json["family"] = address.family
-                address_json["address"] = address.address
-                address_json["netmask"] = address.netmask
-                address_json["broadcast"] = address.broadcast
-                address_json["ptp"] = address.ptp
-
-                interface_json["addresses"].append(address_json)
+            interface_json["Name"] = interface
+            try:
+                interface_json["Address"] = interfaces[interface][0].address
+            except:
+                interface_json["Address"] = ''
+            try:
+                interface_json["MacAddress"] = interfaces[interface][2].address
+            except:
+                interface_json["MacAddress"] = ''
+            try:
+                interface_json["IP"] = interfaces[interface][1].address
+            except:
+                interface_json["IP"] = ''
 
             interfaces_json.append(interface_json)
 
