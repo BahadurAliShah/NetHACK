33b64d80152caceebb8a6395a4dad6b56572febf
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-28 21:21:58 +0500

    Devices Function added

--- Backend/Packages/sniffer.py
+++ Backend/Packages/sniffer.py
@@ -6,6 +6,8 @@ from flask_socketio import emit
 
 temp_packets = []
 Packets = []
+Devices = []
+IPs = []
 Continue = False
 LOCK = threading.Lock()
 DELAY = 0.5
@@ -75,11 +77,24 @@ class Sniffer:
             packet_json["Capture_length"] = ""
 
         try:
-            packet_json["Frame_protocols"] = [ layer.name for layer in self.__get_packet_layers(packet) ]
-        #     get the security protocols
-
+            packet_json["Frame_protocols"] = ["Frame_info", ]
+            temp_layers = [layer.name for layer in self.__get_packet_layers(packet) ]
+            new_layers = []
+            for layer in temp_layers:
+                counter = 1
+                if temp_layers.count(layer) > 1:
+                    for i in new_layers:
+                        try:
+                            if int(i.split("_")[-1]):
+                                counter = int(i.split("_")[-1]) + 1
+                        except:
+                            pass
+                    new_layers.append(layer + "_" + str(counter))
+                else:
+                    new_layers.append(layer)
+            packet_json["Frame_protocols"].extend(new_layers)
         except:
-            packet_json["Frame_protocols"] = ""
+            packet_json["Frame_protocols"] = []
         try:
             packet_json["Frame_checksum"] = packet.chksum
         except:
@@ -96,7 +111,7 @@ class Sniffer:
 
     def __create_packet_json(self, packet):
         packet_json = {}
-        packet_json["layers"] = {}
+        packet_json["Frame_info"] = (self.__get_frame_info_json(packet))
 
         for layer in self.__get_packet_layers(packet):
             layer_json = {}
@@ -111,28 +126,66 @@ class Sniffer:
             # except:
             #     pass
             # print(layer_json["fields"])
-            packet_json["layers"][layer.name] = layer_json
-        packet_json["Frame_info"] = (self.__get_frame_info_json(packet))
+            layerName = layer.name
+            for i in packet_json["Frame_info"]["Frame_protocols"]:
+                try:
+                    temp = i.split("_")[:-1].join("_")
+                    if temp == layerName and i not in packet_json.keys():
+                        layerName = i
+                except:
+                    pass
+            packet_json[layerName] = layer_json
 
         return (packet_json)
 
     def start(self, interface):
         def print_layers(packet):
-            global temp_packets, Packets, Continue
+            global temp_packets, Packets, Continue, Devices, LOCK
             # print(self.__create_packet_json(packet))
 
             packet_json = self.__create_packet_json(packet)
+            if packet_json["Frame_info"]["deviceType"] == "Unicast":
+                try:
+                    device = {
+                        "Mac Address": packet_json["Ethernet"]["src"],
+                        "Interface": packet_json["Frame_info"]["interfaceId"],
+                        "IP Address": packet_json["IP"]["src"],
+                        "Connected to": [packet_json["Ethernet"]["dst"], ]
+                    }
+                    deviceAlreadyExists = False
+                    for i in Devices:
+                        if i["Mac Address"] == device["Mac Address"]:
+                            deviceAlreadyExists = True
+                            if device["Mac Address"] not in i["Connected to"] and device["Mac Address"] != i["Mac Address"]:
+                                i["Connected to"].append(device["Mac Address"])
+                            break
+
+
+                    if packet_json["IP"]["src"] not in IPs:
+                        IPs.append(packet_json["IP"]["src"])
+                    if packet_json["IP"]["dst"] not in IPs:
+                        IPs.append(packet_json["IP"]["dst"])
+                    if not deviceAlreadyExists:
+                        Devices.append(device)
+                except:
+                    pass
             LOCK.acquire()
             temp_packets.append(packet_json)
             Packets.append(packet_json)
             # print(len(temp_packets))
             LOCK.release()
+            print(IPs)
+            print(Devices)
 
         def stopFilter(packet):
             global Continue
             return not Continue
 
-        sniff(prn=print_layers, iface=interface, stop_filter=stopFilter)
+        sniff(prn=print_layers, iface=interface, stop_filter=stopFilter, store=0, promisc=True)
+
+    # def getDevices(self):
+    #     global Packets
+
 
 
 def dataGenerator(interface):
